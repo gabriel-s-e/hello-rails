c0bb58fc4ce015210e6fc6b173c97fa7ee89df2d
Gabriel Serrano <gabrielserrano@tamu.edu>
2025-01-01 09:20:48 +0000

    adding more movies

--- db/seeds.rb
+++ db/seeds.rb
@@ -18,9 +18,15 @@ more_movies = [
   {:title => 'Nomadland', :rating => 'R',
     :release_date => '19-Feb-2021'},
   {:title => 'CODA', :rating => 'PG-13',
-    :release_date => '13-Aug-2021'}
+    :release_date => '13-Aug-2021'},
+  {:title => 'Sonic the Hedgehog 3', :rating => 'PG-13',
+    :release_date => '26-Dec-2024'},
+  {:title => 'Alien:Romulus', :rating => 'R',
+    :release_date => '16-Aug-2024'},
+  {:title => 'Nosferatu', :rating => 'R',
+    :release_date => '25-Dec-2024'}
 ]
 
 more_movies.each do |movie|
-  Movie.create!(movie)
+  Movie.find_or_create_by!(movie)
 end
